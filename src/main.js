68c7c36a8bae37ce15de33aaee02c63cf6c6af68
dixit-star <dixit991752@gmail.com>
2025-12-31 13:21:02 +0530

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,14 @@
+
+// import product from './product.json'
+import { showProductConatiner } from './homeProductCard.js';
+//console.log(product);
+
+fetch("./product.json").then(res=>res.json())
+.then(data=>{
+    // console.log(data);
+    showProductConatiner(data)
+})
+
+// --------------show the temlets-------------
+
+
